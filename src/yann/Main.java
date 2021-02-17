80c1fc7ea8a70aae305428b8587120843caf60ce
Yann <ydauvin@enssat.fr>
2021-02-17 21:11:20 +0100

    nouvelle solution

--- src/yann/Main.java
+++ src/yann/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     public static void main (String[] args) throws IOException {
 
-        //String line1 = readFile();
-        String line1 = "7 2 2 2";
+        String line1 = readFile("b");
+        //String line1 = "8 2 20 1";
         String[] line = line1.split(" ");
         int nbPizza = Integer.parseInt(line[0]);
         int nb2 = Integer.parseInt(line[1]);
@@ -22,7 +22,8 @@ public class Main {
         System.out.println("Equipe de 3 : " + nb3);
         System.out.println("Equipe de 4 : " + nb4);
 
-        int[] livre = nourir(nbPizza,nb2,nb3,nb4);
+        int[] livra = nourir(nbPizza,0,0,0);
+        int[] livre = verif(livra,nb2,nb3,nb4);
 
         System.out.println("============================");
 
@@ -33,45 +34,29 @@ public class Main {
         System.out.println("Je livre donc " + (2*livre[0]+3*livre[1]+4*livre[2]) +" pizza");
     }
 
-    public static int[] nourir(int pizza, int team2, int team3, int team4){
-
-        int livre2=0;
-        int livre3=0;
-        int livre4=0;
-
-        // on cherche le plus de nombre de binome
-        if (pizza % 2 == 0) {
-            livre2 = pizza / 2;
-        } else {
-            livre2 = pizza % 2;
+    public static int[] nourir(int pizza, int livre2, int livre3, int livre4){
+        int livre[] = {livre2, livre3, livre4};
+        if(pizza==0){
+            return livre;
         }
-
-        int betc = pizza - 2 * livre2;
-
-        //avec ce qu'il reste on crée des equipes de 3
-        if (betc % 3 == 0) {
-            livre3 = betc / 3;
-        } else {
-            //si le nombre n'set pas divisible par 3
-            livre2 --;
-            betc= betc + 2;
-            livre3 = betc / 3;
-        }
-
-        livre4 = (pizza - 2 * livre2 - 3 * livre3)/4;
-        //on enleve des equipes de 2 pour mettre des 4
-        int tmp = livre2/2;
-        while (tmp>0){
-            if (livre2 / 2 > 0) {
-                livre4 ++;
-                livre2 = livre2 - 2;
+        else {
+            if(pizza%2==0){
+                return nourir(pizza - 2,livre2+1,livre3,livre4);
+            }else {
+                return nourir(pizza-3,livre2,livre3+1,livre4);
             }
-            tmp--;
         }
+    }
 
-        System.out.println("a "+livre2);
-        System.out.println("b "+livre3);
-        System.out.println("c "+livre4);
+    public static int[] verif(int[] livre,int team2,int team3,int team4){
+        int livre2 = livre[0];
+        int livre3 = livre[1];
+        int livre4 = livre[2];
+
+        while (livre2>=2){
+            livre4++;
+            livre2= livre2 -2 ;
+        }
 
         while (livre4>team4){
             livre4--;
@@ -83,18 +68,15 @@ public class Main {
             livre3=livre3+2;
         }
 
-        int livre[] = {livre2,livre3,livre4};
-
-        return livre;
+        int[] livraison = {livre2, livre3, livre4};
+     return livraison;
     }
 
-
-
-    public static String readFile() throws IOException {
+    public static String readFile(String input) throws IOException {
 
         String line1 = null;
         int nb=0;
-        BufferedReader in = new BufferedReader(new FileReader("input/a.in"));
+        BufferedReader in = new BufferedReader(new FileReader("input/"+input+".in"));
         String line;
         while ((line = in.readLine()) != null)
         {
