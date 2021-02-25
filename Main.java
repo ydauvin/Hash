ed9e0b5959bc606718ea99ac0966c8de765af224
julie <jboudebs@enssat.fr>
2021-02-25 21:11:48 +0100

    algonaif pseudo code

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static InterList interlist = new InterList();
+
+    public static void main (String[] args)  {
+    }
+}
---
+++ src/algo/Algo.java
@@ -0,0 +1,34 @@
+class Etat
+{
+    int temps
+    int interserction
+    int inter_vert
+}
+
+public class Algo 
+{
+    ArrayList<Etat> reponse
+    
+
+ public void algotrèsnaif() 
+ {
+    fin = True;
+    while(fin)
+    {
+
+    }
+    //Pour chaque temps
+        //Pour chaque noeud
+            //Pour chaque intersection entrante
+                //Pour chaque voiture *
+                    //calcul de ce qui reste à parcourir *
+                //calcul de la moyenne *
+            //Selection de l'intersection avec la plus grande moyenne *
+            //Vert pour l'intersection sélectionnée
+ }
+
+ public void algomoinsnaif() {
+     
+ }
+
+}
