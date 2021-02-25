42e36e07def3dcfaf1ec1f8fcbb3d4b02dc10e46
mallou <maelle.lenoble@hotmail.fr>
2021-02-25 20:06:46 +0100

    List_inter

---
+++ src/InterList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class InterList {
+    public ArrayList<Intersection> intersections;
+
+    public InterList() {
+        this.intersections = new ArrayList<>();
+    }
+
+    public Intersection getbyid(int id){
+        for (int i = 0; i < intersections.size(); i++) {
+            Intersection inter = this.intersections.get(i)
+            if (inter.getId() == id){
+                return inter;
+                break;
+            }
+
+        }
+    }
+
+    public ArrayList<Intersection> getIntersections() {
+        return intersections;
+    }
+
+    public void setIntersections(ArrayList<Intersection> linter) {
+        this.intersections = linter;
+    }
+}
