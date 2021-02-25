acee59a2dc6c0c742d46327a2b3ac35607ed6d34
mallou <maelle.lenoble@hotmail.fr>
2021-02-25 22:19:21 +0100

    Etat

--- src/algo/Algo.java
+++ src/algo/Algo.java
@@ -1,29 +1,61 @@
-class Etat
-{
-    int temps
-    int interserction
-    int inter_vert
+package algo;
+
+import model.InterList;
+
+import java.util.ArrayList;
+
+class Etat {
+    int temps;
+    int interserction;
+    String rue;
+
+    public Etat(int temps, int interserction, String rue) {
+        this.temps = temps;
+        this.interserction = interserction;
+        this.rue = rue;
+    }
+
+    public int getTemps() {
+        return temps;
+    }
+
+    public void setTemps(int temps) {
+        this.temps = temps;
+    }
+
+    public int getInterserction() {
+        return interserction;
+    }
+
+    public void setInterserction(int interserction) {
+        this.interserction = interserction;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
 }
 
 public class Algo 
 {
-    InterList inters
-    ArrayList<Etat> reponse
+    InterList inters;
+    ArrayList<Etat> reponse;
     
-    public void Algo(Interlist inters)
+    public void Algo(InterList inters)
     {
-        this.inters=inters
+        this.inters=inters;
     }
- public void algotrèsnaif() 
- {
-    fin = true;
-    while(fin)
+    public void algotrèsnaif()
     {
-        for inter in this.inters.getIn()
+        boolean fin = true;
+        while(fin)
         {
-            
+            //for inter in this.inters.getIn(){}
         }
-    }
     //Pour chaque temps
         //Pour chaque noeud
             //Pour chaque intersection entrante
@@ -32,10 +64,10 @@ public class Algo
                 //calcul de la moyenne *
             //Selection de l'intersection avec la plus grande moyenne *
             //Vert pour l'intersection sélectionnée
- }
+    }
 
  public void algomoinsnaif() {
-     
+
  }
 
 }
